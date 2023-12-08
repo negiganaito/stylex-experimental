614efcc98a70faafe78c705c77ca1a5c0c0736d2
tienlx97 <tienlx97@gmail.com>
2023-12-08 15:45:59 +0700

    chore: custom theme

---
+++ lib/CometStyleXSheet.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+import { StyleXSheet } from './StyleXSheet';
+export declare class CometStyleXSheet extends StyleXSheet {
+  constructor(props: any);
+}
+export declare const rootStyleSheet: any;
+export declare const DARK_MODE_CLASS_NAME: any;
+export declare const LIGHT_MODE_CLASS_NAME: any;
